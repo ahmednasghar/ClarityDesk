f85a41af507ddec0e4f68d8ae4bb8aca62850a02
ahmednasghar <ahmednasghar@gmail.com>
2025-06-28 17:00:11 -0400

    Updated Code

--- Code/claritydesk.ino/claritydesk.ino.ino
+++ Code/claritydesk.ino/claritydesk.ino.ino
@@ -1,6 +1,6 @@
 int brightnessanalog =A5;
 int brightnessval;
-int brakeTime = 250; // For reference: 1000 = 1 second
+int brakeTime = 500; // For reference: 1000 = 1 second
 int redled = 10;
 int yellowled = 11;
 int greenled = 12;
@@ -25,6 +25,12 @@ DHT dht(DHTPIN,DHTTYPE);
 unsigned long tempreadlast=0;
 unsigned long tempinterval = 5000;
 
+//LCD DISPLAY
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(8, 5, 4, 3, A0, A2);
+int newbutton;
+int brightnessreaderPin = A1;  
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -41,25 +47,39 @@ pinMode(buzzerPin,OUTPUT);
 //Temp
 dht.begin();
 
+//LCD DISPLAY
+lcd.begin(16, 2);
+lcd.print("Starting System...");
+delay(1500);
+lcd.clear();
+
+
+
 }
-void LedBlinker(int ledlight){
-  digitalWrite(ledlight,HIGH);
-  delay(brakeTime);
-  digitalWrite(ledlight,LOW);
-  delay(brakeTime);
-}
+
+
+
 void loop() {
 //LDR  
 brightnessval = analogRead(brightnessanalog); // This allows the variable brightnessval to hold the value of the brightness measured by the A1 analog
 Serial.println(brightnessval);
-if(brightnessval>=300){
-  LedBlinker(greenled);
+if(brightnessval >= 300){
+  digitalWrite(greenled, HIGH);
+  delay(brakeTime);
+  digitalWrite(greenled, LOW);
+  delay(brakeTime);
 }
-if(brightnessval<300 && brightnessval>100){
-  LedBlinker(yellowled);
+if(brightnessval< 300 && brightnessval>100){
+  digitalWrite(yellowled, HIGH);
+  delay(brakeTime);
+  digitalWrite(yellowled, LOW);
+  delay(brakeTime);
 }
 if(brightnessval<=100){
-  LedBlinker(redled);
+  digitalWrite(redled, HIGH);
+  delay(brakeTime);
+  digitalWrite(redled, LOW);
+  delay(brakeTime);
 }
 
 
@@ -112,4 +132,45 @@ if(isnan(temp) || isnan(humidity)){
   Serial.println(" %");
 }
 tempreadlast = millis();
+
+//LCD DISPLAY
+newbutton = analogRead(brightnessreaderPin);
+// Convert timer to minutes to seconds
+unsigned long timeLeft = (timeChecker) ? (timelength - (millis() - timestart)) / 1000 : 0;
+int minutes = timeLeft / 60;
+int seconds = timeLeft % 60;
+
+// Refreshs the LCD every loop
+lcd.setCursor(0, 0);
+lcd.print("Temp:");
+lcd.print(temp);
+lcd.print((char)223); // Degree symbol
+lcd.print("F ");
+
+if (newbutton > 300) {
+  lcd.print("HGH");
+} else if (newbutton >= 100 && newbutton < 300) {
+  lcd.print("MED"); 
+} else {
+  lcd.print("LOW");
+}
+
+
+lcd.print("Lvl:");
+lcd.print(newbutton); 
+
+lcd.setCursor(0, 1);
+lcd.print("Timer:");
+lcd.print(timeChecker ? "ON " : "OFF");
+lcd.print(" ");
+
+if (timeChecker) {
+  if (minutes < 10) lcd.print("0");
+  lcd.print(minutes);
+  lcd.print(":");
+  if (seconds < 10) lcd.print("0");
+  lcd.print(seconds);
+} else {
+  lcd.print("       "); // clears the time thats leftover
+}
 }
