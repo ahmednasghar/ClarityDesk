45eda65d8730b8e033c21c6eb8b700e0a2b9476b
ahmednasghar <ahmednasghar@gmail.com>
2025-06-27 23:30:41 -0400

    Added working button timer, temp sensor, and LDR logic

--- Code/claritydesk.ino/claritydesk.ino.ino
+++ Code/claritydesk.ino/claritydesk.ino.ino
@@ -1,44 +1,115 @@
-// Setting up variables for the Brightness (LDR) Functionality
-int brightnessanalog = A1;
-int brightnesslvl;
-int redled;
-int yellowled;
-int greenled;
-int breaktime=500; //For reference 1000 is 1 second
+int brightnessanalog =A5;
+int brightnessval;
+int brakeTime = 250; // For reference: 1000 = 1 second
+int redled = 10;
+int yellowled = 11;
+int greenled = 12;
 
+// Button Timer Variables
+const int buttonPin=9;
+int beforebuttonval=HIGH;
+int buttonval=HIGH;
+int redPin=7;
+int buzzerPin=6;
+int redBlink=300;
+bool timeChecker=false;
+unsigned long timestart=0;
+unsigned long timelength=15000; //ATM its 15 secods for the sake of testing - Will change
+int buttondelay=250;
 
+//Temp Checker Variables
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN,DHTTYPE);
+unsigned long tempreadlast=0;
+unsigned long tempinterval = 5000;
 
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
-
 // LDR
 pinMode(brightnessanalog,INPUT);
 pinMode(redled,OUTPUT);
 pinMode(yellowled,OUTPUT);
 pinMode(greenled,OUTPUT);
-
+//Button
+pinMode(buttonPin,INPUT_PULLUP);
+pinMode(redPin,OUTPUT);
+pinMode(buzzerPin,OUTPUT);
+//Temp
+dht.begin();
 
 }
-// This lets us make a function so we dont repeat ourself a million times
 void LedBlinker(int ledlight){
   digitalWrite(ledlight,HIGH);
-  delay(breaktime);
+  delay(brakeTime);
   digitalWrite(ledlight,LOW);
-  delay(breaktime);
+  delay(brakeTime);
 }
 void loop() {
 //LDR  
-brightnesslvl = analogRead(brightnessanalog); // This allows the variable brightnesslvl to hold the value of the brightness measured by the A1 analog
-Serial.println(brightnesslvl); //Allows us to see the real time brightness of the environment
-delay(breaktime); 
-if(brightnesslvl>=500){
+brightnessval = analogRead(brightnessanalog); // This allows the variable brightnessval to hold the value of the brightness measured by the A1 analog
+Serial.println(brightnessval);
+if(brightnessval>=300){
   LedBlinker(greenled);
 }
-if(brightnesslvl>=500){
+if(brightnessval<300 && brightnessval>100){
   LedBlinker(yellowled);
 }
-if(brightnesslvl>=500){
+if(brightnessval<=100){
   LedBlinker(redled);
+}
+
+
+
+
+// Button Timer
+buttonval = digitalRead(buttonPin);
+delay(buttondelay);  // small delay to debounce and slow down Serial Monitor
+Serial.println(buttonval);
+
+if (buttonval == LOW && beforebuttonval == HIGH) {
+  if (timeChecker == false) {
+    timeChecker = true;
+    timestart = millis();
+    Serial.println("The Timer Has Started!");
+    delay(300); // helps with bounce and accidental double triggers
+  } else {
+    timeChecker = false;
+    timestart = 0;
+    Serial.println("The Timer Has Stopped");
+    delay(300); // also debounce stop press
+  }
+}
+
+if (timeChecker && (millis() - timestart >= timelength)) {
+  for (int j = 0; j < 5; j++) {
+    digitalWrite(redPin, HIGH);
+    digitalWrite(buzzerPin, HIGH);
+    delay(redBlink);
+    digitalWrite(redPin, LOW);
+    digitalWrite(buzzerPin, LOW);
+    delay(redBlink);
+  }
+  Serial.println("Time is UP!");
+  timeChecker = false;
+  timestart = 0;
+}
+
+beforebuttonval = buttonval;
+//Temp
+float temp = dht.readTemperature(true);
+float humidity= dht.readHumidity();
+if(isnan(temp) || isnan(humidity)){
+  Serial.println("ERROR!! Unable to read sesor");
+} else{
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print(" F | Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+}
+tempreadlast = millis();
 }
